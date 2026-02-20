7ed205e3628980254a8a0e5329f29e848b571062
Yhon Vivas <yhonvivas16@gmail.com>
2026-02-20 11:56:20 -0300

    agrego linea en el database.py

--- app/db/database.py
+++ app/db/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=True, connect_args={"prepared_statement_cache_size": 0})
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(DeclarativeBase):
